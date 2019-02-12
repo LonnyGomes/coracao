5d2bba102e9e5dbcc7099f3a67223be0efabef10
Lonny Gomes <code@lonnygomes.com>
2019-02-12 15:55:39 -0500

    Add basic counter to keep state

---
+++ count.js
@@ -0,0 +1,25 @@
+
+const path = require('path');
+const fs = require('fs-extra');
+const filename = path.resolve(__dirname, 'count.json');
+const countConfig =
+
+module.exports = class Count {
+
+    loadCount() {
+        return fs.readJSONSync(filename);
+    }
+
+    async saveCount(newVal) {
+        await fs.writeJSONSync(filename, {total: newVal});
+
+        return this.loadCount();
+    }
+
+    async incrementCount() {
+        const curCount = await this.loadCount();
+        curCount.total += 1;
+
+        return this.saveCount(curCount.total);
+    }
+};
