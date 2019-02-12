573a00ed58c65faa1cb002c891cdb7bf29d47dad
Lonny Gomes <code@lonnygomes.com>
2019-02-12 16:01:02 -0500

    Add koa router

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const Koa = require('koa');
+var Router = require('koa-router');
+var router = new Router();
 const app = new Koa();
+
 const Count = require('./count');
 const count = new Count();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-  return count.incrementCount();
+router.get('/', (ctx, next) => {
+    ctx.body = count.loadCount();
 });
+
+router.get('/increment', (ctx, next) => {
+    ctx.body = count.incrementCount();
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000);
