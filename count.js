9e29030938b2c6fca47f7d624652df399c88a568
Lonny Gomes <code@lonnygomes.com>
2019-02-13 15:40:47 -0500

    Add update display method to talk to pipe

--- count.js
+++ count.js
@@ -1,17 +1,16 @@
-
 const path = require('path');
 const fs = require('fs-extra');
 const filename = path.resolve(__dirname, 'count.json');
-const countConfig =
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 module.exports = class Count {
-
     loadCount() {
         return fs.readJSONSync(filename);
     }
 
     async saveCount(newVal) {
-        await fs.writeJSONSync(filename, {total: newVal});
+        await fs.writeJSONSync(filename, { total: newVal });
 
         return this.loadCount();
     }
@@ -19,7 +18,15 @@ module.exports = class Count {
     async incrementCount() {
         const curCount = await this.loadCount();
         curCount.total += 1;
+        await this.saveCount(curCount.total);
 
-        return this.saveCount(curCount.total);
+        return this.updateDisplay(curCount.total);
+    }
+
+    async updateDisplay(countVal) {
+        const val = isNaN(countVal) ? 0 : countVal;
+        const cmd = `echo -n '${val}' > /tmp/countpipe`;
+
+        return await exec(cmd);
     }
 };
